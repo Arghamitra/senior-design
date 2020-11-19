570d04a31afbdb79da68c58a8136160ca46e3c4d
argha <Arghamira.talukder@tamu.edu>
2020-11-19 02:00:57 -0500

    worked on the validation

--- pydca-revert-1-master/pydca-revert-1-master/examples/demo.py
+++ pydca-revert-1-master/pydca-revert-1-master/examples/demo.py
@@ -11,20 +11,24 @@ from pydca.msa_trimmer import msa_trimmer
 from pydca.contact_visualizer import contact_visualizer
 from pydca.dca_utilities import dca_utilities
 import time
+from pydca.fasta_reader import fasta_reader
+
 st_time = time.time()
 
-rna_msa_file = '/home/at/work/Senior Design Project/pydca-revert-1-master/pydca-revert-1-master/examples/combined_MSA_ddi_1.fasta'
-#rna_msa_file = '/home/at/work/Senior Design Project/pydca-revert-1-master/pydca-revert-1-master/examples/MSA_RF00167_Trimmed.fa'
-#rna_refseq_file = '/home/at/work/Senior Design Project/pydca-revert-1-master/pydca-revert-1-master/examples/combined_MSA_ddi_1_ref.fasta'#
-#rna_refseq_file = '/home/at/work/Senior Design Project/pydca-revert-1-master/pydca-revert-1-master/examples/ref_RF00167.fa'
+#rna_msa_file = '/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/1B70_A_1B70_B.fas'
+rna_msa_file = '/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/MSA_RF00167.fa'
+#rna_msa_file  = '/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/combined_MSA_ddi_1.fasta'
+rna_refseq_file = '/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/ref_RF00167.fa'
+    #'/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/ref_RF00167.fa'
+#'/home/at/work/senior-design/pydca-revert-1-master/pydca-revert-1-master/examples/MSA_RF00167_Trimmed.fa'
 
 # create MSATrimmer instance
 trimmer = msa_trimmer.MSATrimmer(
-    rna_msa_file, biomolecule='protein', #refseq_file=rna_refseq_file,
+    rna_msa_file, biomolecule='rna', refseq_file=rna_refseq_file,
     )
 #refseq_file=rna_refseq_file,
-#trimmed_data = trimmer.get_msa_trimmed_by_refseq(remove_all_gaps=True)
-trimmed_data = trimmer.get_msa_trimmed_by_gap_size(remove_all_gaps=True)
+trimmed_data = trimmer.get_msa_trimmed_by_refseq(remove_all_gaps=True)
+#trimmed_data = trimmer.get_msa_trimmed_by_gap_size(remove_all_gaps=True)
 #trimmed_data = trimmer.get_msa()
 
 #write trimmed msa to file in FASTA format
@@ -36,7 +40,7 @@ with open(trimmed_data_outfile, 'w') as fh:
 
 plmdca_inst = plmdca.PlmDCA(
     trimmed_data_outfile,
-    'protein'
+    'rna'
     '',
     seqid = 0.8,
     lambda_h = 1.0,
@@ -52,7 +56,7 @@ plmdca_FN_APC = plmdca_inst.compute_sorted_FN_APC()
 #create mean-field DCA instance
 mfdca_inst = meanfield_dca.MeanFieldDCA(
     trimmed_data_outfile,
-    'protein',
+    'rna',
     pseudocount = 0.5,
     seqid = 0.8,
 
@@ -75,7 +79,8 @@ print(time.time()-st_time)
 
 #exit(0)
 
-plmdca_visualizer = contact_visualizer.DCAVisualizer('protein', 'x', '1y26',
+plmdca_visualizer = contact_visualizer.DCAVisualizer('rna', 'x', '1y26',
+#plmdca_visualizer = contact_visualizer.DCAVisualizer('rna', 'i', '9PEZ',
     refseq_file = rna_refseq_file,
     sorted_dca_scores = plmdca_FN_APC,
     linear_dist = 4,
@@ -83,7 +88,7 @@ plmdca_visualizer = contact_visualizer.DCAVisualizer('protein', 'x', '1y26',
 )
 contact_map_data = plmdca_visualizer.plot_contact_map()
 tp_rate_data = plmdca_visualizer.plot_true_positive_rates()
-mfdca_visualizer = contact_visualizer.DCAVisualizer('protein', 'x', '1y26',
+mfdca_visualizer = contact_visualizer.DCAVisualizer('rna', 'x', '1y26',
     refseq_file = rna_refseq_file,
     sorted_dca_scores = mfdca_FN_APC,
     linear_dist = 4,
