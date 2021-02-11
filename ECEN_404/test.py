8f7f2848df37480fa4114136d9236993bd299473
argha <Arghamira.talukder@tamu.edu>
2021-02-11 03:05:04 -0500

    added vector machine data processing script

--- ECEN_404/test.py
+++ ECEN_404/test.py
@@ -1,8 +1,13 @@
-list1 = ['1', '2', '3', '4']
+#list1 = ['1', '2', '3', '4']
 
 s = ""
 
 # joins elements of list1 by '-'
 # and stores in sting s
-s = s.join(list1)
-print(s)
+#s = s.join(list1)
+#print(s)
+
+import numpy as np
+#data = np.load('/home/at/work/data_processed/protein_train.npy')
+data = np.load('/home/at/work/senior-design/ECEN_404/TRAINING_negatives.npy')
+print("DONE")
